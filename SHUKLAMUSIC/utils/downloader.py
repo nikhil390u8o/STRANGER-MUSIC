9dc2c4aaea80b09fe297dfed7aa5113cbce04e2c
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-08-17 21:19:28 +0530

    Update downloader.py

--- SHUKLAMUSIC/utils/downloader.py
+++ SHUKLAMUSIC/utils/downloader.py
@@ -1,6 +1,9 @@
-from os import path
+from os import path, makedirs
 import yt_dlp
 from yt_dlp.utils import DownloadError
+
+# make sure downloads folder exists
+makedirs("downloads", exist_ok=True)
 
 ytdl = yt_dlp.YoutubeDL(
     {
@@ -9,17 +12,20 @@ ytdl = yt_dlp.YoutubeDL(
         "geo_bypass": True,
         "nocheckcertificate": True,
     }
- )
+)
 
 
 def download(url: str, my_hook) -> str:       
+    # make sure downloads folder exists
+    makedirs("downloads", exist_ok=True)
+
     ydl_optssx = {
-        'format' : 'bestaudio[ext=m4a]',
+        "format": "bestaudio[ext=m4a]",
         "outtmpl": "downloads/%(id)s.%(ext)s",
         "geo_bypass": True,
         "nocheckcertificate": True,
-        'quiet': True,
-        'no_warnings': True,
+        "quiet": True,
+        "no_warnings": True,
     }
     info = ytdl.extract_info(url, False)
     try:
@@ -27,8 +33,7 @@ def download(url: str, my_hook) -> str:
         x.add_progress_hook(my_hook)
         dloader = x.download([url])
     except Exception as y_e:
-        return print(y_e)
-    else:
-        dloader
+        print(y_e)
+        return None
     xyz = path.join("downloads", f"{info['id']}.{info['ext']}")
     return xyz
